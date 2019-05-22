34dc058a02534e79ec79fdfbf09277632fa627d7
shuchang <80003674@sfmail.sf-express.com>
2019-05-22 20:19:10 +0800

    121

--- src/ConflictTest.java
+++ src/ConflictTest.java
@@ -1,7 +1,7 @@
 public class ConflictTest {
     public static void main(String[] args) {
         System.out.println(123);
-        System.out.println("你好世界");
+        System.out.println("曾经沧海");
     }
 
 }
