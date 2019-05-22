67c831ff45067d9c0df8c98b3a50818a3ff4ed75
shuchang <80003674@sfmail.sf-express.com>
2019-05-22 20:58:21 +0800

    121

--- src/ConflictTest.java
+++ src/ConflictTest.java
@@ -1,12 +1,17 @@
+import java.util.HashMap;
+
 public class ConflictTest {
     public static void main(String[] args) {
         System.out.println("今日头条");
         System.out.println(66666666);
         System.out.println(123);
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
+
+        String a = "abcd";
+        HashMap map = new HashMap();
+        map.put("haha",a);
+        map.put("1","10");
+
         System.out.println("我是小明");
         System.out.println("张三");
         System.out.println("李四");
