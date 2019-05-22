ca278b9c8dfc80c7d640e2fe4334dc236016fae2
shuchang <80003674@sfmail.sf-express.com>
2019-05-22 20:35:47 +0800

    121

--- src/ConflictTest.java
+++ src/ConflictTest.java
@@ -1,5 +1,6 @@
 public class ConflictTest {
     public static void main(String[] args) {
+        System.out.println("今日头条");
         System.out.println(123);
         System.out.println("我是小明");
         System.out.println("张三");
