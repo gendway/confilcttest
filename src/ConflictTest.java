1ff3006cbfa8f4d3e82ba1ae7dc5438e8687ba40
shuchang <80003674@sfmail.sf-express.com>
2019-05-22 20:40:30 +0800

    111

--- src/ConflictTest.java
+++ src/ConflictTest.java
@@ -1,14 +1,10 @@
 public class ConflictTest {
     public static void main(String[] args) {
-        System.out.println("今日头条");
-        System.out.println(123);
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+
+        for (int i = 0; i < 88; i++) {
+            System.out.println("重新再来");
         }
-        System.out.println("我是小明");
-        System.out.println("张三");
-        System.out.println("李四");
     }
 
 }
