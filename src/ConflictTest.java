456820efd411303a0e93b52d5a9944bac3c31f10
shuchang <80003674@sfmail.sf-express.com>
2019-12-27 16:23:32 +0800

    version1

--- src/ConflictTest.java
+++ src/ConflictTest.java
@@ -1,8 +1,25 @@
+import java.util.HashMap;
+
+import java.util.HashMap;
+
 public class ConflictTest {
     public static void main(String[] args) {
         System.out.println("今日头条");
         System.out.println(66666666);
         System.out.println(123);
+
+        for (int i = 0; i < 1000000; i++) {
+            System.out.println("web frame is detected web 检测到web框架");
+        }
+
+        String a = "abcd";
+        HashMap map = new HashMap();
+        map.put("haha",a);
+        map.put("1","10");
+        System.out.println(1231233);
+        for (int i = 0; i < 88; i++) {
+            System.out.println("重新再来");
+        }
 
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
