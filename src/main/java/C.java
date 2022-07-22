df6e38757146594de43ddf371f8dd9de98b957ff
chang.shu <chang.shu@speedaf.com>
2022-07-22 11:12:12 +0800

    C类提交

---
+++ src/main/java/C.java
@@ -0,0 +1,12 @@
+/**
+ * @Description:
+ * @Author: chang.shu
+ * @Date: 2022/7/22/022
+ */
+public class C {
+
+    public static void main(String[] args) {
+        System.out.println("C类");
+    }
+
+}
